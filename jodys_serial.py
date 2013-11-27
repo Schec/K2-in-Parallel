24e9f6aa9577ef50c83f555e4f603731bbf7dc00
Schec <jodyschechter@gmail.com>
2013-11-27 09:02:13 -0500

    jodys serial code

---
+++ jodys_serial.py
@@ -0,0 +1,116 @@
+from __future__ import division
+import numpy as np
+import itertools
+import pandas as pd
+import math
+import operator
+
+aEH93 = np.array([[1,0,0],[1,1,1],[0,0,1],[1,1,1],[0,0,0],[0,1,1],[1,1,1],[0,0,0],[1,1,1],[0,0,0]])
+
+
+
+def vals_of_attributes(D,n):
+    output = []
+    for i in xrange(n):
+        output.append(list(np.unique(D[:,i])))
+    return output
+
+def alpha(df, mask):
+    _df = df
+    for combo in mask:
+        _df = _df[_df[combo[0]] == combo[1]]  # I know there must be a way to speed this up - but i couldn't find it
+    return len(_df)
+
+def f(i,pi,attribute_values,df):
+
+    len_pi = len(pi)
+
+    phi_i_ = [attribute_values[item] for item in pi]
+    if len(phi_i_) == 1:
+        phi_i = [[item] for item in phi_i_[0]]
+    else:
+        phi_i = list(itertools.product(*phi_i_))
+
+    # bug fix: phi_i might contain empty tuple (), which shouldn't be counted in q_I
+    try:
+        phi_i.remove(())
+    except ValueError:
+        pass
+
+    q_i = len(phi_i)
+
+    V_i = attribute_values[i]
+    r_i = len(V_i)
+
+    product = 1
+    numerator = math.factorial(r_i - 1)
+
+    # special case: q_i = 0
+    if q_i == 0:
+        js = ['special']
+    else:
+        js = range(q_i) 
+
+    for j  in js:
+
+        # initializing mask to send to alpha
+        if j == 'special':
+            mask = []
+        else:
+            mask = zip(pi,phi_i[j])
+
+        # initializing counts that will increase with alphas
+        N_ij = 0
+        inner_product = 1
+
+        for k in xrange(r_i):
+            # adjusting mask for each k
+            mask_with_k = mask + [[i,V_i[k]]]
+            alpha_ijk = alpha(df,mask_with_k)
+            N_ij += alpha_ijk
+            inner_product = inner_product*math.factorial(alpha_ijk)
+        denominator = math.factorial(N_ij + r_i - 1)
+        product = product*(numerator/denominator)*inner_product
+    return product
+
+
+def k2(D,node_order,u=2):
+    n = D.shape[1]
+    m = D.shape[0]
+    attribute_values = vals_of_attributes(D,n)
+
+    df = pd.DataFrame(D)
+    OKToProceed = False
+    parents = []
+
+    for i in xrange(n):
+        OKToProceed = False
+        pi = []
+        pred = node_order[0:i]
+        P_old = f(i,pi,attribute_values,df)
+        if len(pred) > 0:
+            OKToProceed = True
+        while (OKToProceed == True and len(pi) < u):
+            iters = [item for item in pred if item not in pi]
+            if len(iters) > 0:
+                f_to_max = {};
+                for z_hat in iters:
+                    f_to_max[z_hat] = f(i,pi+[z_hat],attribute_values,df)
+                z = max(f_to_max.iteritems(), key=operator.itemgetter(1))[0]
+                P_new = f_to_max[z]
+                if P_new > P_old:
+                    P_old = P_new
+                    pi = pi+[z]
+                else:
+                    OKToProceed = False
+            else:
+                OKToProceed = False
+        parents.append(pi)
+
+    print parents
+    
+    return parents
+
+
+
+k2(aEH93,[0,1,2])

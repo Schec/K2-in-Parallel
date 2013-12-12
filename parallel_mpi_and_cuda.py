b78360d5ea7d56882332b7bdc36921266b8b502e
Schec <jodyschechter@gmail.com>
2013-12-12 18:32:55 -0500

    cleaned parallel_mpi_and_cuda.py

--- parallel_mpi_and_cuda.py
+++ parallel_mpi_and_cuda.py
@@ -2,17 +2,13 @@ from __future__ import division
 import numpy as np
 import itertools
 import pandas as pd
-import math
 import operator
-import time
 import sys
 import argparse
 import pickle
 
 from mpi4py import MPI
 
-import pycuda.autoinit
-import pycuda.driver as cu
 import pycuda.gpuarray as gpu
 import pycuda.compiler as nvcc
 
@@ -61,33 +57,36 @@ __global__ void my_f(int* d_combinations, float* d_res, int* d_ll,int* d_DF,int 
 
 ########## FUNCTIONS ##########
 
+
 ##### Calculating Unique Values of Features in the Dataset #####
-def vals_of_attributes(D,n):
+def vals_of_attributes(D, n):
     output = []
     for i in xrange(n):
-        output.append(list(np.unique(D[:,i])))
+        output.append(list(np.unique(D[:, i])))
     return output
 
+
 ##### Evaluating the f function at initialization #####
-def f_initialization(i,attribute_values,df):
+def f_initialization(i, attribute_values, df):
 
     V = attribute_values[i]
     r = len(V)
 
     #Calculate alpha_0 and alpha_1
-    alpha_0 = df[df[i]==0].shape[0]
-    alpha_1 = df[df[i]==1].shape[0]
+    alpha_0 = df[df[i] == 0].shape[0]
+    alpha_1 = df[df[i] == 1].shape[0]
     N = alpha_0 + alpha_1
 
     numerator = np.sum([np.log(b) for b in range(1, r)])
-    denominator = np.sum([np.log(b) for b in range(1, r+N)])
-    term1 = sum([np.log(x) for x in range(1,alpha_0+1)])
-    term2 = sum([np.log(x) for x in range(1,alpha_1+1)])
+    denominator = np.sum([np.log(b) for b in range(1, r + N)])
+    term1 = sum([np.log(x) for x in range(1, alpha_0 + 1)])
+    term2 = sum([np.log(x) for x in range(1, alpha_1 + 1)])
 
     return numerator - denominator + term1 + term2
 
+
 ##### Evaluating the f function for all the combinations in parallel #####
-def f_combinations(i,pi,attribute_values,df,f_kernel):
+def f_combinations(i, pi, attribute_values, df, f_kernel):
 
     #Finding the Combinations
     phi_i_ = [attribute_values[item] for item in pi]
@@ -98,98 +97,91 @@ def f_combinations(i,pi,attribute_values,df,f_kernel):
     df_height = np.int32(df.shape[0])
     nf = np.int32(df[pi].shape[1])
     height = np.int32(combinations.shape[0])
-    block_x = np.int(height/nf)
-    blocksize = (block_x,1,1)
-    gridsize = (1,1)
+    block_x = np.int(height / nf)
+    blocksize = (block_x, 1, 1)
+    gridsize = (1, 1)
 
     #Apply Kernel
-    h_DF = np.array(df[pi], dtype = np.int32)
+    h_DF = np.array(df[pi], dtype=np.int32)
     h_DF = np.array(h_DF.ravel().tolist(), dtype=np.int32)
     h_ll = np.array(df[i], dtype=np.int32)
-    h_res = np.zeros(height/nf,dtype=np.float32)
+    h_res = np.zeros(height / nf, dtype=np.float32)
     d_combinations = gpu.to_gpu(combinations)
     d_res = gpu.to_gpu(h_res)
     d_ll = gpu.to_gpu(h_ll)
     d_DF = gpu.to_gpu(h_DF)
-    f_kernel(d_combinations,d_res,d_ll, d_DF,nf, height, df_height, block=blocksize,grid=gridsize)
+    f_kernel(d_combinations, d_res, d_ll, d_DF, nf, height, df_height,
+        block=blocksize, grid=gridsize)
 
     ress = d_res.get()
     ress = list(ress)
 
     return sum(ress)
 
+
 ##### Performing the K2 algorithm #####
-def k2_in_parallel(D,node_order,comm,rank,size,f_kernel, u=2):
+def k2_in_parallel(D, node_order, comm, rank, size, f_kernel, u=2):
     n = D.shape[1]
-    assert len(node_order) == n, "Node order is not correct length.  It should have length %r" % n
-    m = D.shape[0]
-    attribute_values = vals_of_attributes(D,n)
+    assert len(node_order) == n, ("Node order is not correct length."
+        "  It should have length %r" % n)
+    attribute_values = vals_of_attributes(D, n)
 
     df = pd.DataFrame(D)
     OKToProceed = False
-
 
     # master node
     if rank == 0:
         parents = {}
         status = MPI.Status()
 
-        for i in range(1,size):
-            comm.send( i - 1, dest = i)
+        for i in range(1, size):
+            comm.send(i - 1, dest=i)
 
-        for i in xrange(size-1,n):
-            new_parents = comm.recv(source = MPI.ANY_SOURCE, status = status)
+        for i in xrange(size - 1, n):
+            new_parents = comm.recv(source=MPI.ANY_SOURCE, status=status)
             parents.update(new_parents)
             destin = status.Get_source()
-            comm.send(i, dest = destin)
+            comm.send(i, dest=destin)
 
-        for i in range(1,size):
-            new_parents = comm.recv(source = MPI.ANY_SOURCE, status = status)
+        for i in range(1, size):
+            new_parents = comm.recv(source=MPI.ANY_SOURCE, status=status)
             parents.update(new_parents)
             destin = status.Get_source()
-            comm.send(None, dest = destin)
+            comm.send(None, dest=destin)
 
-        #print parents
         return parents
 
     # slave nodes
     else:
-#        calculation_time = 0
-#        message_time = 0
-
         while (True):
-#            message_start = time.time()
-            i = comm.recv(source = 0)
-#            message_end = time.time()
-#            message_time += message_end - message_start
+            i = comm.recv(source=0)
 
-            if i == None:
-#                print "node ", rank, " spent ", calculation_time, " seconds calculating parent sets"
-#                print "node ", rank, " spent ", message_time, " seconds sending and receiving messages"
+            if i is None:
                 return
 
             else:
-#                calc_start = time.time()
                 OKToProceed = False
                 pi = []
                 pred = node_order[0:i]
-                P_old = f_initialization(node_order[i],attribute_values,df)
+                P_old = f_initialization(node_order[i], attribute_values, df)
                 if len(pred) > 0:
                     OKToProceed = True
 
-                while (OKToProceed == True and len(pi) < u):
+                while (OKToProceed is True and len(pi) < u):
                     iters = [item for item in pred if item not in pi]
                     if len(iters) > 0:
-                        f_to_max = {};
+                        f_to_max = {}
 
                         for z_hat in iters:
-                            f_to_max[z_hat] = f_combinations(node_order[i],pi+[z_hat],attribute_values,df, f_kernel)
+                            f_to_max[z_hat] = f_combinations(node_order[i],
+                                pi + [z_hat], attribute_values, df, f_kernel)
 
-                        z = max(f_to_max.iteritems(), key=operator.itemgetter(1))[0]
+                        z = max(f_to_max.iteritems(),
+                            key=operator.itemgetter(1))[0]
                         P_new = f_to_max[z]
                         if P_new > P_old:
                             P_old = P_new
-                            pi = pi+[z]
+                            pi = pi + [z]
                         else:
                             OKToProceed = False
                     else:
@@ -197,21 +189,41 @@ def k2_in_parallel(D,node_order,comm,rank,size,f_kernel, u=2):
 
                 message = {node_order[i]: pi}
 
-                comm.send(message, dest = 0)
+                comm.send(message, dest=0)
 
 
 if __name__ == "__main__":
 
-    ##### Use argparse to give directly the input in the command line #####
-    parser = argparse.ArgumentParser(description = '''K2 In Parallel using MPI and CUDA:  Calculates the parent set for each node in your data file and returns a dictionary of the form {feature: [parent set]}.''')
-    parser.add_argument('-D', nargs='?', default = None, help='''Path to csv/txt file containing a 0/1 array with m observations (rows) and n features (columns). A value of 1 represents the presence of that feature in that observation. One of --random and -D must be used.''')
-    parser.add_argument('--node_order', '-o', nargs='?',  type = list, default = None, help='''A list of integers containing the column order of features in your matrix. If not provided, order the features in accordance with their order in the file.''')
-    parser.add_argument('--random', '-r', action = "store_true", help='Include this option to calculate parents for a random matrix.  If --random is included, -D and --node_order should be left out, and -m and -n can be included. One of --random and -D must be used.')
-    parser.add_argument('-n', nargs='?', type = int, default = '10', help='The number of features in a random matrix.  default is 10.  Only use with --random')
-    parser.add_argument('-m', nargs='?', type = int, default = '100',  help='The number of observations in a random matrix.  default is 100. only use with --random')
-    parser.add_argument('-u', nargs='?', type = int, default = 2, help='The maximum number of parents per feature.  Default is 2.  Must be less than number of features.')
-    parser.add_argument('--outfile', nargs='?',default=None,help='The output file where the dictionary of {feature: [parent set]} will be written')
-    parser.add_argument('--seed', nargs='?', type=int, default=None,help='The seed for the random matrix.  Only use with --random')
+    parser = argparse.ArgumentParser(description='''K2 In Parallel:  Calculates
+         the parent set for each node in your data file and returns a
+         dictionary of the form{feature: [parent set]}.''')
+    parser.add_argument('-D', nargs='?', default=None, help='''Path to csc file
+         containing a 0/1 array with m observations (rows) and n features
+         (columns).  A value of 1 represents the presence of that feature in
+         that observation. One of --random and -D must be used.''')
+    parser.add_argument('--node_order', '-o', nargs='?', type=list,
+        default=None, help='''A list of integers containing the column order
+        of features in your matrix.  If not provided, order the features in
+        accordance with their order in the file.''')
+    parser.add_argument('--random', '-r', action="store_true",
+        help='''Include this option to calculate parents for a random matrix.
+        If --random is included, -D and --node_order should be left out, and
+        -m, --seed, and -n can be included.   One of --random and -D ust be
+        used.''')
+    parser.add_argument('--seed', nargs='?', type=int, default=None,
+            help='The seed for the random matrix.  Only use with --random')
+    parser.add_argument('-n', nargs='?', type=int, default='10',
+            help='''The number of features in a random matrix.
+            Default is 10.  Only use with --random''')
+    parser.add_argument('-m', nargs='?', type=int, default='100',
+        help='''The number of observations in a random matrix.
+        Default is 100. Only use with --random''')
+    parser.add_argument('-u', nargs='?', type=int, default=2,
+        help='''The maximum number of parents per feature.  Default is 2.
+                Must be less than number of features.''')
+    parser.add_argument('--outfile', nargs='?', default=None, help='''The
+         output file where the dictionary of {feature: [parent set]} will be
+         written''')
     args = parser.parse_args()
 
     ##### Load Kernel and MPI Size and Ranks #####
@@ -228,23 +240,19 @@ if __name__ == "__main__":
         n = args.n
         m = args.m
         if rank == 0:
-	    if args.seed is not None:
-              np.random.seed(args.seed)
-            D = np.random.binomial(1,0.7,size=(m,n))
+            if args.seed is not None:
+                np.random.seed(args.seed)
+            D = np.random.binomial(1, 0.9, size=(m, n))
         else:
             D = None
         D = comm.bcast(D, root=0)
         node_order = list(range(n))
 
-    elif not args.D == None:
-        if rank == 0:
-            print "Reading in array D"
+    elif args.D is not None:
         D = np.loadtxt(open(args.D))
-        if args.node_order != None:
+        if args.node_order is not None:
             node_order = args.node_order
         else:
-            if rank == 0:
-                print "Determining node order"
             n = np.int32(D.shape[1])
             node_order = list(range(n))
 
@@ -253,26 +261,23 @@ if __name__ == "__main__":
             print "Incorrect usage. Use --help to display help."
         sys.exit()
 
-    if rank == 0:
-        print "Calculating Parent sets"
     comm.barrier()
     start = MPI.Wtime()
-
-    #### MPI+CUDA K2 Algorithm #####
-    parents = k2_in_parallel(D,node_order,comm,rank,size,f_kernel,u=u)
-
+    parents = k2_in_parallel(D, node_order, comm, rank, size, f_kernel, u=u)
     comm.barrier()
     end = MPI.Wtime()
 
     ##### Outputs #####
     if rank == 0:
-        print "Parallel computing time", end-start
-        print parents
-	if args.outfile!=None:
-	  f = open(outfile,'w')
-	  try:
-	    pickle.dump(parents,f)
-	  except RaiseError:
-	    for key,item in parents.iteritems():
-	      strr = str(key)+' '+str(item)+'\n'
-	      f.write(strr)
+        print "Parallel computing time", end - start
+
+        if args.outfile is not None:
+            out = open(outfile, 'w')
+            try:
+                pickle.dump(parents, out)
+            except RuntimeError:
+                for key, item in parents.iteritems():
+                    strr = str(key) + ' ' + str(item) + '\n'
+                    out.write(strr)
+        else:
+            print parents
